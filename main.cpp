441f824f73af91e6250b4cc150184a2246d7f929
Florian Dehe <florian.dehe@gmail.com>
2023-07-22 02:32:58 +0200

    Vulkan base setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include "glfw/include/GLFW/glfw3.h"
+#include <cstdint>
 #include <cstdio>
 #include <iostream>
+#include <stdint.h>
 #include <vulkan/vulkan_core.h>
 
+#define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
-#define GLFW_EXPOSE_NATIVE_WAYLAND
+#define GLFW_EXPOSE_NATIVE_X11
 #include "GLFW/glfw3native.h"
 
 #include "bgfx/bgfx.h"
@@ -14,9 +18,15 @@
 #define W_HEIGHT 900
 
 GLFWwindow *window;
+static bgfx::ViewId mainViewId = 0;
 
 void error_callback(int error, const char* description){
   fprintf(stderr, "Error: %s\n", description);
+}
+
+void resize_callback(GLFWwindow *window, int width, int height) {
+  bgfx::reset((uint32_t)width, (uint32_t)height, BGFX_RESET_VSYNC);
+  bgfx::setViewRect(mainViewId, 0, 0, bgfx::BackbufferRatio::Equal);
 }
 
 int main(int argc, char *argv[]) {
@@ -40,32 +50,39 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  // Passing native window to bgfx & init
-  bgfx::PlatformData pd;
-  pd.nwh = glfwGetWaylandWindow(window);
-  bgfx::setPlatformData(pd);
-
   bgfx::Init bgfxInit;
+  // Graphics Lib
   bgfxInit.type = bgfx::RendererType::Vulkan;
+
+  // Wayland
+  // bgfxInit.platformData.nwh = (void*) (std::uintptr_t) glfwGetWaylandWindow(window);
+  // bgfxInit.platformData.ndt = glfwGetWaylandDisplay();
+
+  // X11
+  bgfxInit.platformData.nwh = (void*) (std::uintptr_t) glfwGetX11Window(window);
+  bgfxInit.platformData.ndt = glfwGetX11Display();
+
+  // Resolution
   bgfxInit.resolution.width = W_WIDTH;
   bgfxInit.resolution.height = W_HEIGHT;
   bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
-  if (!bgfx::init()) {
-    std::cerr << "BGFX Init failed !\n";
-    return -1;
-  }
 
-  std::cout << "made it!\n";
+  bgfx::init(bgfxInit);
+
+  //
+  glfwSetFramebufferSizeCallback(window, resize_callback);
 
   // Clear
-  bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355ff, 1.f, 0);
-  std::cout << "made it!\n";
+  bgfx::setViewClear(mainViewId, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355ff, 1.f, 0);
   bgfx::setViewRect(0, 0, 0, W_WIDTH, W_HEIGHT);
-  std::cout << "made it!\n";
 
   // Game Loop
   unsigned int counter = 0;
   while(!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    bgfx::touch(mainViewId);
+
     bgfx::frame();
     counter++;
   }
